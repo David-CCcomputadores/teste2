6bc32b46c8db1e79d55ac9a5214ed74f55893a63
a18987 <a18987@d1-08-07.dped.local>
2017-02-07 15:17:36 +0000

    Versão Alpha 0.1

---
+++ src/teste2/Main.java
@@ -0,0 +1,15 @@
+package teste2;
+
+/**
+ *
+ *@author David e Aléxio
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("Olá Mundo");
+        //Fiz Código
+ 
+    }
+
+}
