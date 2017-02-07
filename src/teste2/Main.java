4002c9d12c5b761f83df1c5604953ee5ab9a0b97
a18987 <a18987@d1-08-07.dped.local>
2017-02-07 15:29:29 +0000

    Versão Alpha 0.1.2

--- src/teste2/Main.java
+++ src/teste2/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
        System.out.println("Olá Mundo");
         //Fiz Código
+        System.out.println("Adeus Mundo");
+        //Fiz Código
  
     }
 
